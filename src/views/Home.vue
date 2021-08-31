8e01879d927fae5e980176c7b67b34cc679dfd15
RafaelDiasCampos <rafaeldc.99@gmail.com>
2021-08-31 14:17:10 -0300

    Melhorada tela Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="home pa-10">
-    <v-img
-      :src="require('../assets/logo.png')"
-      class="my-3"
-      contain
-      height="200"
-      left
-    />
-    <span v-for="section in sections" :key="section.title">
-      <h1 class="display-2 pl-8">{{ section.title }}</h1>
-      <p class="pt-5">{{ section.text }}</p>
-    </span>
+    <v-container>
+      <v-img
+        :src="require('../assets/logo.png')"
+        class="my-3"
+        contain
+        height="200"
+        left
+      />
+      <span v-for="section in sections" :key="section.title">
+        <h1 class="display-2 pl-8">{{ section.title }}</h1>
+        <p class="pt-5">{{ section.text }}</p>
+      </span>
+    </v-container>
   </div>
 </template>
 
@@ -20,11 +22,20 @@ export default {
   data() {
     return {
       sections: [
-        {title: "Quem Somos", text: "A ClinicaSaúde é uma clínica médica especializada em atendimento de alta qualidade e acessível."},
-        {title: "Nossa missão", text: "Nossa missão é trazer atendimento médico de forma acessível para todas as pessoas."},
-        {title: "Nossos valores", text: "Acreditamos que a saúde vem antes de tudo. Dessa forma, é necessário um atendimento médico para todos"}
-      ]
-    }
-  }
+        {
+          title: "Quem Somos",
+          text: "Fundada em 2021. a ClinicaSaúde é uma clínica médica especializada em atendimento de alta qualidade e acessível. Somos um pequeno grupo de pessoas muito dedicadas e atenciosas, dando o nosso melhor para ajudar quem precisa.",
+        },
+        {
+          title: "Nossa missão",
+          text: "Nossa missão é trazer atendimento médico de forma acessível para todas as pessoas. Isso só é possível com muita força de vontade e ajuda de diversas pessoas.",
+        },
+        {
+          title: "Nossos valores",
+          text: "Acreditamos que a saúde vem antes de tudo, e precisa ser fornecida para todos e todas. Também acreditamos que a honestidade e compaixão são fundamentais em qualquer situação.",
+        },
+      ],
+    };
+  },
 };
 </script>
